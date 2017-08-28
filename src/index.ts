9fadcb4477f0cf626e6fda43b099ab9c43313f5f
Yu Chen <yu.chen@live.ie>
2017-08-29 00:54:22 +0800

    fixes ConnectionBackend and rest

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,6 @@ export class AuthLocalModule {
     return {
       ngModule: AuthLocalModule,
       providers: [
-        Http,
         {
           provide: DefaultAuthLocalPaths,
           useValue: options
